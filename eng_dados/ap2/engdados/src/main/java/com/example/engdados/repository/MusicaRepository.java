3427477897cac593a877fd821fe405c765023bfb
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2023-11-17 00:30:35 -0300

    repository find

--- eng_dados/ap2/engdados/src/main/java/com/example/engdados/repository/MusicaRepository.java
+++ eng_dados/ap2/engdados/src/main/java/com/example/engdados/repository/MusicaRepository.java
@@ -4,6 +4,11 @@ import com.example.engdados.model.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MusicaRepository extends JpaRepository<Musica, Integer> {
+    List<Musica> findByCategoriaId(Integer id);
+
+    List<Musica> findMusicasByAutoresId(Integer id);
 }
