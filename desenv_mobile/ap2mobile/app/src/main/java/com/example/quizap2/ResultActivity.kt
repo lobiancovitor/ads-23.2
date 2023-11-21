dac358b0b96545e096132e2b12a749759d7071cb
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2023-11-21 18:06:45 -0300

    onClick to pokedex

--- desenv_mobile/ap2mobile/app/src/main/java/com/example/quizap2/ResultActivity.kt
+++ desenv_mobile/ap2mobile/app/src/main/java/com/example/quizap2/ResultActivity.kt
@@ -1,10 +1,12 @@
 package com.example.quizap2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +20,19 @@ class ResultActivity : AppCompatActivity() {
         val resultTv: TextView = findViewById(R.id.resultTv)
         val scoreTv: TextView = findViewById(R.id.scoreTv)
         val btnRestart: Button = findViewById(R.id.btnRestart)
+        val btnPokemonWebsite = findViewById<Button>(R.id.btnPokemonWebsite)
 
         resultTv.text = "Resultado de $userName!"
         scoreTv.text = "Você acertou $score de $totalQuestions"
         btnRestart.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            //finish()
+        }
+
+        btnPokemonWebsite.setOnClickListener {
+            val pokemonWebsiteUrl = "https://www.pokemon.com/br/pokedex/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(pokemonWebsiteUrl))
+            startActivity(intent)
         }
     }
 }
